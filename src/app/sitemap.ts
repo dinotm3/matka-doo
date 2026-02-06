bb9443dca68c60baa307ef45494b0d3811219781
dinotm <dinotm3@gmail.com>
2026-02-06 06:33:34 +0100

    sitemap update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,23 +11,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 1,
     },
-    {
-      url: `${BASE_URL}/#usluge`,
-      lastModified,
-      changeFrequency: "monthly",
-      priority: 0.9,
-    },
-    {
-      url: `${BASE_URL}/#location`,
-      lastModified,
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${BASE_URL}/#contact`,
-      lastModified,
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
   ];
 }
